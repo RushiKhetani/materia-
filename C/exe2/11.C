7785d52849621068a660236de3997571ebb65117
Rushi Khetani <70133197+RushiKhetani@users.noreply.github.com>
2022-02-03 10:38:46 +0530

    Add files via upload

---
+++ C/exe2/11.C
@@ -0,0 +1,42 @@
+#include<stdio.h>
+#include<conio.h>
+
+void main()
+{
+	int i,n,j;
+	char s1[20],s[20];
+	printf("enter data for revrse: ");
+	gets(s);
+
+	for(i=0; s[i] != '\0'; i++);
+		n=i-1;
+
+	for(j=0; j<i; j++)
+	{
+		  s1[j] = s[n];
+		  n--;
+	}
+	s1[j] = '\0';
+	printf("%s",s1);
+}
+
+// or
+//#include<stdio.h>
+//#include<string.h>
+//
+//int main()
+//{
+//	int i,j,n=0;
+//	char str[10],str2[10];
+//	
+//	printf("Enter s string: ");
+//	gets(str);
+//		
+//	for(i=0; str[i]!='\0'; i++)
+//		n++;
+//		
+//	for(j=n-1; j>=0; j--)
+//	{
+//		printf("%c",str[j]);	
+//	}
+//}
